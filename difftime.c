a93fef0b0ce6b476c95c8dfb09d38f1dd73c3fc0
Kapparov <31594319+Kapparov@users.noreply.github.com>
2017-11-02 14:17:39 +0100

    Update difftime.c

--- difftime.c
+++ difftime.c
@@ -24,7 +24,7 @@ int main (void)
                 printf("Execution time: %.0i minutes %i seconds\n", diff_t / 60, diff_t % 60);
                 exit(0);
         }
-        if (diff_t >= 25) {
+        if (diff_t >= 60) {
                 printf("Execution time: %.0i hours %.0i minutes %.0i seconds\n", diff_t / 3600, (diff_t / 60) % 3600 - (diff_t / 3600) * 60, diff_t % 60);
         }
         return(0);
